db1581c7ecf41cebb99e9caa519c365a90410145
Hassan Ahmed Khan <ihassanahmedkhan@gmail.com>
2023-12-09 22:47:54 +0500

    image-not-showing-2

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import logo from "../../../public/images/logo/logo.png"
 
 function Logo() {
   return (
     <div>
-      <Image src={'/images/logo/logo.png'} alt='logo' width={120} height={120}/>
+      <Image src={logo} alt='logo' width={120} height={120}/>
     </div>
   )
 }
